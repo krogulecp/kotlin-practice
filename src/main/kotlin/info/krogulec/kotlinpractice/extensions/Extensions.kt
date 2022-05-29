99c7b43fb28ba25640dee206771e3ac59b4293e8
Piotr Krogulec <piotr.krogulec@allegro.pl>
2022-05-29 15:27:36 +0200

    cezar cipher

--- src/main/kotlin/info/krogulec/kotlinpractice/extensions/Extensions.kt
+++ src/main/kotlin/info/krogulec/kotlinpractice/extensions/Extensions.kt
@@ -1,5 +1,16 @@
 package info.krogulec.kotlinpractice.extensions
 
 fun String.encodeWithCesarCipher(shift: Int): String {
-    TODO()
+    return this
+        .map { letter -> Char(letter.code + shift) }
+        .joinToString("")
+}
+
+
+fun String.printHello(name: String) {
+    println("$name hello $this")
+}
+
+fun main() {
+    "some string".printHello("John")
 }
