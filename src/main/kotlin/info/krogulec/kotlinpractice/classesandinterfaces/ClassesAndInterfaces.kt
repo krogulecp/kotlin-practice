696bfbd3c47461e5c63edce78fdc4c19e9e59006
Piotr Krogulec <piotr.krogulec@allegro.pl>
2022-05-29 10:23:08 +0200

    classes and interfaces

---
+++ src/main/kotlin/info/krogulec/kotlinpractice/classesandinterfaces/ClassesAndInterfaces.kt
@@ -0,0 +1,86 @@
+package info.krogulec.kotlinpractice.classesandinterfaces
+
+fun main() {
+    val classesAndInterfaces = ClassesAndInterfaces()
+    ClassesAndInterfaces.NestedClass()
+    classesAndInterfaces.NestedInnerClass().testNestedInner()
+
+}
+
+class ClassesAndInterfaces private constructor(
+    val param: String
+    //tutaj nie może być kodu (tylko properties)
+) {
+
+    constructor() : this("") {
+        //teoretycznie tutaj możemy wywołać jakiś kod
+    }
+
+    init {
+        println("first init block")
+    }
+
+    init {
+        println("second init block")
+    }
+
+    init {
+        println("third init block")
+    }
+
+    fun classCanHaveManyFunctions() {
+
+    }
+
+    class NestedClass {
+        fun testNested() {
+
+        }
+    }
+
+    inner class NestedInnerClass {
+        fun testNestedInner() {
+            println("Klasa inner ma dostęp do parametrów klasy zewnętrznej $param")
+        }
+    }
+
+    companion object {}
+
+    object MySingleton {
+
+    }
+}
+
+abstract class MyAbstractClass() {
+    // działa podobnie jak w javie
+}
+
+interface MyInterface {
+    val propToImplement: String
+    // val propWithDefaultImpl: String = "" // not allowed in kotlin
+
+    fun toImplement(): Int
+    fun withDefaultImpl() {
+
+    }
+
+    companion object {
+        const val CONST = ""
+    }
+}
+
+class SomeClassImplementingInterface : MyInterface {
+    override val propToImplement = "mm property implementation"
+
+    override fun toImplement(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class SomeClassImplementingInterfaceVer2(override val propToImplement: String) : MyInterface {
+    override fun toImplement(): Int {
+        TODO("Not yet implemented")
+    }
+}
+
+class SomeClassExtendingAbstractClass : MyAbstractClass()
