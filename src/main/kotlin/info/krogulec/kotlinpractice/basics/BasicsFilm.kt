87318689ca91a7f476235c9210b3d2a6bb2e54c3
Piotr Krogulec <piotr.krogulec@allegro.pl>
2022-10-08 11:31:23 +0200

    basicsFilm

--- src/main/kotlin/info/krogulec/kotlinpractice/basics/BasicsFilm.kt
+++ src/main/kotlin/info/krogulec/kotlinpractice/basics/BasicsFilm.kt
@@ -1,3 +1,28 @@
 package info.krogulec.kotlinpractice.basics
 
-class BasicsFilm() {}
+fun main() {
+    val basicFilm = BasicsFilm("Rambo", 1984)
+    basicFilm.addSequels(BasicsFilm("Rambo 2", 1990))
+    println(basicFilm.sequels)
+}
+
+class BasicsFilm(val title: String, private val year: Int) {
+    var available = true
+    private var isPlaying = false
+    val sequels = mutableListOf<BasicsFilm>()
+
+    fun play() {
+        if (available) {
+            isPlaying = true
+            println("playing $title")
+        }
+    }
+
+    fun addSequels(vararg sequelsToAdd: BasicsFilm) {
+        sequels.addAll(sequelsToAdd)
+    }
+
+    override fun toString(): String {
+        return title
+    }
+}
