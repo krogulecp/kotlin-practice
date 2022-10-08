d3b2b14ff479abf23f7324f8fa7bdc3315095a44
Piotr Krogulec <piotr.krogulec@allegro.pl>
2022-10-08 12:50:50 +0200

    types p2

--- src/main/kotlin/info/krogulec/kotlinpractice/types/LazyLogicalOperators.kt
+++ src/main/kotlin/info/krogulec/kotlinpractice/types/LazyLogicalOperators.kt
@@ -4,37 +4,34 @@ import java.lang.Thread.sleep
 
 fun main(){
     //sprawdzić dla użytkowników 73041622793 i 72060398474
-    LazyLogicalOperators().userCanWork("73041622793")
+    val lazyLogicalOperators = LazyLogicalOperators(PoliceService(), UserService())
+
+    //not adult
+    println("User 73041622793 can work -> ${lazyLogicalOperators.userCanWork("73041622793")}")
+
+    //is adult and has criminal record
+    println("User 72060398474 can work -> ${lazyLogicalOperators.userCanWork("72060398474")}")
+
+    //is adult and does not have criminal record
+    println("User 63091729861 can work -> ${lazyLogicalOperators.userCanWork("63091729861")}")
+
+    //pesel not found
+    println("User 0000000000 can work -> ${lazyLogicalOperators.userCanWork("0000000000")}")
+
 }
 
-
-class LazyLogicalOperators {
-
-    fun userCanWork(pesel: String){
-        // TODO zaimplementować logikę -> user może pracować jeśli jest pełnoletni i nie ma przeszłości kryminalnej
-    }
-
-    private fun getUserAge(pesel: String): Int{
-        // zaimplementować pobranie wieku
-        TODO()
-    }
-
-    private fun isAdult(userAge: Int): Boolean{
-        // zaimplementować weryfikację pełnoletności
-        TODO()
-    }
-
-    private fun userHasCriminalRecord(pesel: String): Boolean {
-        // sprawdzić, czy user ma przeszłość kryminalną
-        TODO()
-    }
+class LazyLogicalOperators(
+    private val policeService: PoliceService,
+    private val userService: UserService,
+) {
+    fun userCanWork(pesel: String) = (isAdult(getUserAge(pesel))) && !userHasCriminalRecord(pesel)
+    private fun getUserAge(pesel: String) = userService.getUserAge(pesel) ?: throw RuntimeException("User not found!")
+    private fun isAdult(userAge: Int) = userAge >= 18
+    private fun userHasCriminalRecord(pesel: String) = policeService.getAllUserCrimes(pesel).isNotEmpty()
 }
 
 class UserService {
-    fun getUserAge(pesel: String): Int? {
-        // zaimplementować pobranie wieku
-        TODO()
-    }
+    fun getUserAge(pesel: String) = userDb[pesel]
 
     companion object {
         private val userDb: Map<String, Int> = mapOf(
